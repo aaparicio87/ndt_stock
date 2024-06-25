608ce64af075727d195e672657612c9ceb4aef24
aaparicio87 <alaparicio87@gmail.com>
2024-06-25 13:12:45 +0200

    Add more fields to create work form

--- src/components/MultiSelect/MultiSeleect.tsx
+++ src/components/MultiSelect/MultiSeleect.tsx
@@ -5,6 +5,7 @@ export const MultiSeleect: FC<
     Omit<MultiSelectProps, 'onChange' | 'value'> &
     Partial<Pick<MultiSelectProps, 'onChange' | 'value'>>
 > = ({ onChange: _onChange, value: _value, options: __options, ...props }) => {
+
     const { value, options, onChange } = useMultiSelect({
         value: _value ?? (props.single ? '' : []),
         options: __options!,
